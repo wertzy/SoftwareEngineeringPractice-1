de30928d7c3c0a475e3916e601b6c1abc301dd22
sradms0 <sradms0@openmailbox.org>
2020-02-17 16:27:48 -0500

    change testBank object to store test object from static CentralBank.getTestObject method and fix invalid password on new account

--- src/test/java/edu/ithaca/dragon/bank/ATMUITest.java
+++ src/test/java/edu/ithaca/dragon/bank/ATMUITest.java
@@ -2,13 +2,13 @@ package edu.ithaca.dragon.bank;
 
 public class ATMUITest {
     public static void main(String[] args) throws FrozenAccountException, InsufficientFundsException {
-        CentralBank testBank=new CentralBank();
+        CentralBank testBank=CentralBankTest.getTestObject();
         BasicAPI testBaseAPI=testBank;
         ATMUI testUI=new ATMUI(testBaseAPI);
 
 
         //Desmond's Test
-        BankAccount tb=new BankAccount("a@mail.com","abc","checking",500);
+        BankAccount tb=new BankAccount("a@mail.com","abcdef@1","checking",500);
         testUI.displayOptions(tb);
     }
 }
